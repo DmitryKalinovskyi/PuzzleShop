887e3102374388207f7fc1b58091212d4858fc27
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-01-27 15:41:41 +0200

    IRepository interface dynamic type definition

--- PuzzleShop/Repository/Interfaces/IRepository.cs
+++ PuzzleShop/Repository/Interfaces/IRepository.cs
@@ -1,6 +1,8 @@
 ﻿namespace PuzzleShop.Repository.Interfaces
 {
-    
+    /// <summary>
+    /// Interface defines the basic CRUD operations 
+    /// </summary>
     public interface IRepository
     {
         TEntity? GetById<TEntity, TKeyType> (TKeyType key) where TEntity : class;
@@ -17,27 +19,4 @@
 
         void Save();
     }
-
-
-    // Strong type implementation
-    /// <summary>
-    /// Interface defines the basic CRUD operations 
-    /// </summary>
-    //public interface IRepository<TEntity, TKeyType> where TEntity : class where TKeyType : struct 
-    //{
-    //    TEntity? GetById(TKeyType key);
-    //    ICollection<TEntity> GetAll();
-
-    //    void Insert(TEntity entity);
-    //    void Update(TEntity entity);
-    //    void Delete(TEntity entity);
-
-    //    Task<TEntity?> GetByIdAsync(TKeyType key);
-    //    Task<ICollection<TEntity>> GetAllAsync();
-
-    //    Task InsertAsync(TEntity entity);
-
-    //    void Save();
-    //}
-
 }
