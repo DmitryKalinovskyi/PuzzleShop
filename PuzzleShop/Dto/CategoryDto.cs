fd42a701c7adc5931704251a8c0405cf6c12d2a3
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-01-27 19:43:42 +0200

    Added Dto for each model

---
+++ PuzzleShop/Dto/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PuzzleShop.Dto
+{
+    public class CategoryDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ PuzzleShop/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PuzzleShop.Dto
+{
+    public class UserDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
